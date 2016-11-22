6a060f58b131d3c3a5e81df263e92ce434160bb3
Mohammed Joseph Petrelli Salameh <Mulle@salameh.dk>
2016-11-22 12:45:25 +0100

    Implementation class of Usercontract

---
+++ src/main/java/UserFunctions.java
@@ -0,0 +1,25 @@
+import contract.*;
+import eto.*;
+
+import java.util.Collection;
+import java.util.Date;
+
+
+public class UserFunctions implements UserContract {
+	
+	public Collection<DepartureTime> getAllDepartures(Date date, FerryLineInfo ferryLineInfo) throws InvalidDateException, InvalidRouteException {
+		return null;
+	}
+	
+	public Reservation createReservation(PersonInfo personInfo, FerryLineInfo ferryLineInfo, DepartureTime departureTime, Vehicle vehicle, Number number, Number number1) throws NoVacanciesException, InvalidDateException {
+		return null;
+	}
+	
+	public Number deleteReservation(Number number, String s) throws InvalidReservationIdException, InvalidEmailException, InvalidReservationException {
+		return null;
+	}
+	
+	public Reservation ViewReservation(Number number, String s) throws InvalidReservationIdException, InvalidEmailException, InvalidReservationException {
+		return null;
+	}
+}
